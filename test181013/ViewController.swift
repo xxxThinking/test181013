bbd0085fe931767d7ca112438f4900aa21142abf
xxxThinking <973986153@qq.com>
2018-10-27 10:15:44 +0800

    计算器（-）
    加减乘除的实现
    将输入的字符串转成double类型放入一个成员变量中 在HK(=)函数中 与下一次输入的值 并判断实现方法得到新的值 再转成字符串显示

--- test181013/ViewController.swift
+++ test181013/ViewController.swift
@@ -10,21 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var HX: UITextField!
-    @IBOutlet weak var HY: UITextField!
+    var temp:Double = 0
+    var count = 0
     @IBOutlet weak var HK: UITextField!
+    @IBAction func H1(_ sender: Any) {
+        HK.text = HK.text! + "1"
+    }
+    @IBAction func H2(_ sender: Any) {
+        HK.text = HK.text! + "2"
+    }
+    @IBAction func H3(_ sender: Any) {
+        HK.text = HK.text! + "3"
+    }
+    @IBAction func H4(_ sender: Any) {
+        HK.text = HK.text! + "4"
+    }
+    @IBAction func H5(_ sender: Any) {
+        HK.text = HK.text!  + "5"
+    }
+    @IBAction func H6(_ sender: Any) {
+        HK.text = HK.text! + "6"
+    }
+    @IBAction func H7(_ sender: Any) {
+        HK.text = HK.text! + "7"
+    }
+    @IBAction func H8(_ sender: Any) {
+        HK.text = HK.text! + "8"
+    }
+    @IBAction func H9(_ sender: Any) {
+        HK.text = HK.text! + "9"
+    }
+    @IBAction func H0(_ sender: Any) {
+        HK.text = HK.text! + "0"
+    }
+    @IBAction func SUM(_ sender: Any) {
+        count = 1
+        temp = Double(HK.text!)! //标记数字
+        HK.text = ""
+    }
+    @IBAction func reduce(_ sender: Any) {
+        count = 2
+        temp = Double(HK.text!)! //标记数字
+        HK.text = ""
+    }
+    @IBAction func division(_ sender: Any) {
+        count = 3
+        temp = Double(HK.text!)!
+        HK.text = ""
+    }
+    @IBAction func multiplication(_ sender: Any) {
+        count = 4
+        temp = Double(HK.text!)!
+        HK.text = ""
+    }
     @IBAction func HZ(_ sender: Any) {
-        HK.text = "\(Int(HX.text!)! + (Int)(HY.text!)!)"
+        if(count == 1){
+        temp = temp + Double(HK.text!)!
+            HK.text = "\(temp)"
+        }
+        if(count == 2){
+            temp = temp + Double(HK.text!)!
+            HK.text = "\(temp)"
+        }
+        if(count == 3)
+        {
+            temp = temp / Double(HK.text!)!
+            HK.text = "\(temp)"
+        }
+        if(count == 4)
+        {
+            temp = temp * Double(HK.text!)!
+            HK.text = "\(temp)"
+        }
+    }
+    @IBAction func HAC(_ sender: Any) {
+        HK.text = ""
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
-
-
-
-
