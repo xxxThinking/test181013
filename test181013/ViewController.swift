66b943a07e683bd36c6dc65c899fcb4fcd9c6230
xxxThinking <973986153@qq.com>
2018-11-07 09:29:10 +0800

    运算符号的点击次数
    解决点击运算符号崩溃问题和利用输入值替换0

--- test181013/ViewController.swift
+++ test181013/ViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
 
     var temp:Double = 0
@@ -15,84 +18,157 @@ class ViewController: UIViewController {
     var count = 0
     @IBOutlet weak var HK: UITextField!
     @IBAction func H1(_ sender: Any) {
+        if(HK.text == "0")
+        {
+            HK.text = ""
+        }
         HK.text = HK.text! + "1"
     }
     @IBAction func H2(_ sender: Any) {
+        if(HK.text == "0")
+        {
+            HK.text = ""
+        }
         HK.text = HK.text! + "2"
     }
     @IBAction func H3(_ sender: Any) {
+        if(HK.text == "0")
+        {
+            HK.text = ""
+        }
         HK.text = HK.text! + "3"
     }
     @IBAction func H4(_ sender: Any) {
+        if(HK.text == "0")
+        {
+            HK.text = ""
+        }
         HK.text = HK.text! + "4"
     }
     @IBAction func H5(_ sender: Any) {
+        if(HK.text == "0")
+        {
+            HK.text = ""
+        }
         HK.text = HK.text!  + "5"
     }
     @IBAction func H6(_ sender: Any) {
+        if(HK.text == "0")
+        {
+            HK.text = ""
+        }
         HK.text = HK.text! + "6"
     }
     @IBAction func H7(_ sender: Any) {
+        if(HK.text == "0")
+        {
+            HK.text = ""
+        }
         HK.text = HK.text! + "7"
     }
     @IBAction func H8(_ sender: Any) {
+        if(HK.text == "0")
+        {
+            HK.text = ""
+        }
         HK.text = HK.text! + "8"
     }
     @IBAction func H9(_ sender: Any) {
+        if(HK.text == "0")
+        {
+            HK.text = ""
+        }
         HK.text = HK.text! + "9"
     }
     @IBAction func H0(_ sender: Any) {
         HK.text = HK.text! + "0"
     }
     @IBAction func SUM(_ sender: Any) {
+        if(HK.text == "0" || HK.text == "")
+        {
+            
+        } else
+        {
         count = 1
         temp = Double(HK.text!)!
+        remember = true
         HK.text = ""
+    }
     }
     @IBAction func reduce(_ sender: Any) {
+        if(HK.text == "0" || HK.text == "")
+        {
+            
+        } else
+        {
         count = 2
         temp = Double(HK.text!)!
+         remember = true
         HK.text = ""
+        }
     }
     @IBAction func division(_ sender: Any) {
+        if(HK.text == "0" || HK.text == "")
+        {
+            
+        } else
+        {
         count = 3
         temp = Double(HK.text!)!
+        remember = true
         HK.text = ""
+        }
     }
     @IBAction func multiplication(_ sender: Any) {
+        if(HK.text == "0" || HK.text == "")
+        {
+            
+        } else
+        {
         count = 4
         temp = Double(HK.text!)!
+         remember = true
         HK.text = ""
+        }
     }
     @IBAction func HZ(_ sender: Any) {
+        if(HK.text == "0" || HK.text == "")
+        {
+            
+        } else
+        {
         if(count == 1){
         temp = temp + Double(HK.text!)!
             HK.text = "\(temp)"
+            count = 0
         }
         if(count == 2){
             temp = temp - Double(HK.text!)!
             HK.text = "\(temp)"
+            count = 0
         }
         if(count == 3)
         {
             temp = temp / Double(HK.text!)!
             HK.text = "\(temp)"
+            count = 0
         }
         if(count == 4)
         {
             temp = temp * Double(HK.text!)!
-            temp = Double(HK.text!)!
+            HK.text = "\(temp)"
+            count = 0
+        }
         }
     }
     @IBAction func Hdot(_ sender: Any) {
         if(remember){
          HK.text = HK.text! + "."
-            remember = false
         }
+        remember = false
     }
     @IBAction func HAC(_ sender: Any) {
         HK.text = ""
-        remember = true
     }
     
     override func viewDidLoad() {
